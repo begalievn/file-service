493634652dbf75134dce9613809af180c5673d27
begalievn <begaliev.info@gmail.com>
2022-11-18 18:44:48 +0600

    Adde cust NotFound error

--- controllers/file.controller.js
+++ controllers/file.controller.js
@@ -1,49 +1,46 @@
 const fileService = require('../services/file.service');
 
 class FileController {
-  constructor() {}
-  
-  async getAllFilesFromMongo(req, res) {
-    try {
-      const result = await fileService.getAllFiles();
-      res.send(result);
-    } catch(e) {
-      res.status(500).json(e);
+    constructor() {
     }
-  }
-  
-  async getFile(req, res, next) {
-    try {
-      const result = await fileService.getFile(req.params.filename);
-      if(result) {
-        return result.pipe(res);
-      }
-      res.send(`There is no such a file`);
-    } catch(e) {
-      // res.status(500).json(e);
-      next(e);
+    
+    async getAllFilesFromMongo(req, res) {
+        try {
+            const result = await fileService.getAllFiles();
+            res.send(result);
+        } catch (e) {
+            res.status(500).json(e);
+        }
     }
-  }
-  
-  async postFile(req, res) {
-    try {
-      const data = { ...req.files };
-      console.log("data", data);
-      const result = await fileService.postFile(data);
-      return res.send(result);
-    } catch(e) {
-      res.status(500).json(e);
+    
+    async getFile(req, res, next) {
+        try {
+            const result = await fileService.getFile(req.params.filename);
+            return result.pipe(res);
+        } catch (e) {
+            res.status(e.status).json(e.message);
+        }
     }
-  }
-  
-  async deleteFiles(req, res) {
-    try {
-      const result = await fileService.removeFile(req.params.filename);
-      res.send(result);
-    } catch(e) {
-      res.status(500).json(e);
+    
+    async postFile(req, res) {
+        try {
+            const data = {...req.files};
+            console.log("data", data);
+            const result = await fileService.postFile(data);
+            return res.send(result);
+        } catch (e) {
+            res.status(500).json(e);
+        }
     }
-  }
+    
+    async deleteFiles(req, res) {
+        try {
+            const result = await fileService.removeFile(req.params.filename);
+            res.send(result);
+        } catch (e) {
+            res.status(500).json(e.message);
+        }
+    }
 }
 
 module.exports = new FileController();
--- services/file.service.js
+++ services/file.service.js
@@ -1,8 +1,10 @@
 const fileRepository = require('../repositories/file.repository');
-const { readFile, writeFile } = require('../lib/adapter');
+const {readFile, writeFile} = require('../lib/adapter');
+const NotFoundError = require('../custom-errors/not-found.error');
 
 class FileService {
-    constructor() {}
+    constructor() {
+    }
     
     async getAllFiles() {
         const result = await fileRepository.findAllFiles();
@@ -10,42 +12,36 @@ class FileService {
     }
     
     async getFile(filename) {
-        try {
-            const result = await fileRepository.findFile(filename);
-            if(result) {
-                return await readFile(filename);
-            }
-            return null;
-        } catch(e) {
-            throw new Error("Error while getting file");
+        const result = await fileRepository.findFile(filename);
+        if (!result) {
+            throw new NotFoundError("File not found");
         }
+        return await readFile(filename);
     }
     
     async postFile(data) {
-        
         const newFile = {
             filename: data.fileName,
             mimeType: data.mimeType,
             size: data.size
         }
-    
-        const result = await fileRepository.findAndUpdateOrInsert(newFile);
         
         try {
             const fileWritten = await writeFile(data.fileName, data.buffer);
-        } catch(e) {
+        } catch (e) {
             throw new Error(`Error, file hasn't been written`);
         }
         
+        const result = await fileRepository.findAndUpdateOrInsert(newFile);
         return result;
     }
     
     async removeFile(filename) {
-        if(filename) {
-            const result = fileRepository.findAndRemove(filename);
-            return result;
+        if (!filename) {
+            return `Provide filename`;
         }
-        return `Provide filename`;
+        const result = fileRepository.findAndRemove(filename);
+        return result;
     }
     
 }
